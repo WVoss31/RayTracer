f6e1626f9f90469f7914513c926c38dcd62834e9
Walker Voss <vosswalker@gmail.com>
2024-08-11 14:30:20 -0500

    added basic diffusion and physical based rendering

--- WalnutApp/src/Renderer.cpp
+++ WalnutApp/src/Renderer.cpp
@@ -141,13 +141,13 @@ glm::vec4 Renderer::PerPixel(u_int32_t x, u_int32_t y)
     glm::vec3 color(0.0f);
     float muliplier = 1.0f;
     
-    int bounces = 2;
+    int bounces = 5;
     for (int i = 0; i < bounces; i++)
     {
         Renderer::HitPayload payload = TraceRay(ray);
         if (payload.HitDistance < 0.0f)
         {
-            glm::vec3 skyColor = glm::vec3(0.0f, 0.0f, 0.0f);
+            glm::vec3 skyColor = glm::vec3(0.6f, 0.7f, 0.9f);
             color += skyColor * muliplier;
             break;
         }
@@ -157,15 +157,17 @@ glm::vec4 Renderer::PerPixel(u_int32_t x, u_int32_t y)
         float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -lightDir), 0.0f); // == cos(angle) | cos(> 90) == neg, so we max this
 
         const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
+        const Material& material = m_ActiveScene->Materials[sphere.MaterialIndex];
 
-        glm::vec3 sphereColor = sphere.Albedo;
+        glm::vec3 sphereColor = material.Albedo;
         sphereColor *= lightIntensity;
         color += sphereColor * muliplier;
 
-        muliplier *= 0.7f;
+        muliplier *= 0.5f;
 
         ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.0001f;
-        ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal);
+        ray.Direction = glm::reflect(ray.Direction, 
+            payload.WorldNormal + material.Roughness * Walnut::Random::Vec3(-0.5f, 0.5f));
     }
 
     return glm::vec4(color, 1.0f);
