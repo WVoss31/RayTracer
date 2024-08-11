1a26cb47afd0f7a2ee00aa710386e6f483c17b4e
Walker Voss <vosswalker@gmail.com>
2024-08-10 21:37:07 -0500

    made multiple spheres

--- WalnutApp/src/Renderer.cpp
+++ WalnutApp/src/Renderer.cpp
@@ -14,7 +14,7 @@ namespace Utils {
     }
 }
 
-void Renderer::Render(const Camera& camera)
+void Renderer::Render(const Scene& scene, const Camera& camera)
 {
     Ray ray;
     ray.Origin = camera.GetPosition();
@@ -26,7 +26,7 @@ void Renderer::Render(const Camera& camera)
         {
             ray.Direction = camera.GetRayDirections()[x + y * m_FinalImage->GetWidth()];
 
-            glm::vec4 color = TraceRay(ray);
+            glm::vec4 color = TraceRay(scene, ray);
             color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
             m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 	    }
@@ -55,12 +55,8 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 }
 
 // This is where the colors are determined
-glm::vec4 Renderer::TraceRay(const Ray& ray)
+glm::vec4 Renderer::TraceRay(const Scene& scene, const Ray& ray)
 {
-    float radius = 0.5f;
-    // make a unit vector
-    // rayDirection = glm::normalize(rayDirection);
-
     // Equation for sphere in 3D space
     // (bx^2 + by^2 + bz^2)t^2 + (2(axbx + ayby + azbz))t + (ax^2 + ay^2 + az^2 -r^2) = 0
     // a = ray origin
@@ -71,34 +67,53 @@ glm::vec4 Renderer::TraceRay(const Ray& ray)
 
     // Note: a, b, and c variables represent terms in the equation above
     // e.g. a + b + c = 0
-    
-    // first term (bx^2 + by^2 + bz^2)t^2
-    float a = glm::dot(ray.Direction, ray.Direction); // dot product
-    // middle term (2(axbx + ayby + azbz))t
-    float b = 2.0f * glm::dot(ray.Origin, ray.Direction);
-    // last term (ax^2 + ay^2 + az^2 -r^2)
-    float c = glm::dot(ray.Origin, ray.Origin) - radius * radius;
 
-    // Use Quadratic discriminant to detect intersection with sphere
-    // b^2 - 4ac
-    float discriminant = b * b - 4.0f * a * c;
-
-    if (discriminant < 0.0f)
+    if (scene.Spheres.size() == 0) 
         return glm::vec4(0, 0, 0, 1);
+    
+    const Sphere* closestSphere = nullptr;
+    float hitDistance = FLT_MAX;
+    for (const Sphere& sphere : scene.Spheres)
+    {
+        glm::vec3 origin = ray.Origin - sphere.Position;
 
-    // Solving Quadratic equation (hit distance)
-    // (-b +- sqrt(discriminant)) / 2a
-    // float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
-    float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+        // first term (bx^2 + by^2 + bz^2)t^2
+        float a = glm::dot(ray.Direction, ray.Direction); // dot product
+        // middle term (2(axbx + ayby + azbz))t
+        float b = 2.0f * glm::dot(origin, ray.Direction);
+        // last term (ax^2 + ay^2 + az^2 -r^2)
+        float c = glm::dot(origin, origin) - glm::pow(sphere.Radius, 2);
 
-    glm::vec3 hitPoint = ray.Origin + ray.Direction * closestT; // t1 will always be closest hit
+        // Use Quadratic discriminant to detect intersection with sphere
+        // b^2 - 4ac
+        float discriminant = b * b - 4.0f * a * c;
+
+        if (discriminant < 0.0f)
+            continue;
+
+        // Solving Quadratic equation (hit distance)
+        // (-b +- sqrt(discriminant)) / 2a
+        // float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);  CURRENTLY UNUSED
+        float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+        if (closestT < hitDistance)
+        {
+            hitDistance = closestT;
+            closestSphere = &sphere;
+        }
+    }
+
+    if (closestSphere == nullptr)
+        return glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    glm::vec3 origin = ray.Origin - closestSphere->Position;
+    glm::vec3 hitPoint = origin + ray.Direction * hitDistance;
     glm::vec3 normal = glm::normalize(hitPoint); // Don't need to subtract sphere origin because its (0, 0)
 
     glm::vec3 lightDir = glm::normalize(glm::vec3(-1, -1, -1));
 
     float facingLightRatio = glm::max(glm::dot(normal, -lightDir), 0.0f); // == cos(angle) | cos(> 90) == neg, so we max this
 
-    glm::vec3 sphereColor(1, 0, 1);
+    glm::vec3 sphereColor= closestSphere->Albedo;
     sphereColor *= facingLightRatio;
 
     return glm::vec4(sphereColor, 1.0f);
