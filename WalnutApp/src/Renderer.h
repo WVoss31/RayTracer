c8fa9c9a859bbf8523a5fc53817f348221abbac4
Walker Voss <vosswalker@gmail.com>
2024-08-18 17:28:24 -0500

    copied his version, and its faster. Don't know why, they look identical

--- WalnutApp/src/Renderer.h
+++ WalnutApp/src/Renderer.h
@@ -1,25 +1,32 @@
 #pragma once
 
-#include <memory>
-
 #include "Walnut/Image.h"
-#include "glm/glm.hpp"
 
 #include "Camera.h"
 #include "Ray.h"
 #include "Scene.h"
 
+#include <memory>
+#include <glm/glm.hpp>
+
 class Renderer
 {
-private:
-    std::shared_ptr<Walnut::Image> m_FinalImage;
-    uint32_t* m_ImageData = nullptr;
-    uint32_t m_FrameIndex = 1;
-    const Scene* m_ActiveScene = nullptr;
-    const Camera* m_ActiveCamera = nullptr;
-    glm::vec4* m_AccumulationData = nullptr;
-    std::vector<uint16_t> m_ImageHorizontalIter, m_ImageVerticalIter;
+public:
+    struct Settings
+    {
+        bool Accumulate = true;
+    };
+public:
+    Renderer() = default;
 
+    void OnResize(uint32_t width, uint32_t height);
+    void Render(const Scene& scene, const Camera& camera);
+
+    std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
+
+    void ResetFrameIndex() { m_FrameIndex = 1; }
+    Settings& GetSettings() { return m_Settings; }
+private:
     struct HitPayload
     {
         float HitDistance;
@@ -29,29 +36,22 @@ private:
         int ObjectIndex;
     };
 
-    glm::vec4 PerPixel(uint32_t x, uint32_t y); //RayGen Shader
+    glm::vec4 PerPixel(uint32_t x, uint32_t y); // RayGen
 
     HitPayload TraceRay(const Ray& ray);
-    HitPayload ClosestHit(const Ray& ray, float hitDistance, int objectIndex) const;
-
+    HitPayload ClosestHit(const Ray& ray, float hitDistance, int objectIndex);
     HitPayload Miss(const Ray& ray);
-public:
-    Renderer() = default;
-
-    struct Settings
-    {
-        bool Accumulate = true;
-    };
-
-    void Render(const Scene& scene, const Camera& camera);
-    void OnResize(uint32_t width, uint32_t height);
-
-    void ResetFrameIndex() { m_FrameIndex = 1; }
-    Settings& GetSettings() { return m_Settings; }
-    std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
 private:
+    std::shared_ptr<Walnut::Image> m_FinalImage;
     Settings m_Settings;
 
-};
+    std::vector<uint32_t> m_ImageHorizontalIter, m_ImageVerticalIter;
 
-// #include "Renderer.cpp"
+    const Scene* m_ActiveScene = nullptr;
+    const Camera* m_ActiveCamera = nullptr;
+
+    uint32_t* m_ImageData = nullptr;
+    glm::vec4* m_AccumulationData = nullptr;
+
+    uint32_t m_FrameIndex = 1;
+};
